aaeb3948e5dbaa27ab35313be3d842d662b676db
Emma Turner <mail@emturner.co.uk>
2023-10-18 17:36:18 +0100

    outboxer: display current tezos level
    Display the current tezos level within the UI

--- outboxer/lib/outboxer_web/controllers/page_live.ex
+++ outboxer/lib/outboxer_web/controllers/page_live.ex
@@ -2,6 +2,38 @@ defmodule OutboxerWeb.PageLive do
   use OutboxerWeb, :live_view
 
   def mount(_params, _conn, socket) do
+    socket = assign(socket, tezos_level: "")
+    if connected?(socket), do: Process.send_after(self(), :constants, 100)
+
     {:ok, socket}
+  end
+
+  def handle_info(:constants, socket) do
+    proto_constants = proto_constants()
+    socket = assign(socket, constants: proto_constants)
+    Process.send_after(self(), :tezos_level, 100)
+    {:noreply, socket}
+  end
+
+  def handle_info(:tezos_level, socket) do
+    block_time = socket.assigns.constants.block_time_ms
+    level = tezos_level()
+    socket = assign(socket, tezos_level: level)
+    Process.send_after(self(), :tezos_level, block_time)
+
+    {:noreply, socket}
+  end
+
+  def proto_constants() do
+    %HTTPoison.Response{body: body} = HTTPoison.get! "http://localhost:20000/chains/main/blocks/head-1/context/constants"
+    %{"minimal_block_delay" => block_time} = Poison.decode! body
+    {block_time, _} = Integer.parse block_time
+    %{block_time_ms: block_time * 1000}
+  end
+
+  def tezos_level() do
+    %HTTPoison.Response{body: body} = HTTPoison.get! "http://localhost:20000/chains/main/blocks/head-1/header/shell"
+    %{"level" => level} = Poison.decode! body
+    level
   end
 end
--- outboxer/mix.exs
+++ outboxer/mix.exs
@@ -47,7 +47,9 @@ defmodule Outboxer.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:httpoison, "~> 2.0"},
+      {:poison, "~> 5.0"}
     ]
   end
 
