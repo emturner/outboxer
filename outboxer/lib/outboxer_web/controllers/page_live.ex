68b3402e946ab591ee23167decd2880346f4abdf
Emma Turner <mail@emturner.co.uk>
2023-10-19 21:27:54 +0100

    app: display full outboxes

--- outboxer/lib/outboxer_web/controllers/page_live.ex
+++ outboxer/lib/outboxer_web/controllers/page_live.ex
@@ -2,7 +2,7 @@ defmodule OutboxerWeb.PageLive do
   use OutboxerWeb, :live_view
 
   def mount(_params, _conn, socket) do
-    socket = assign(socket, tezos_level: "", rollup_address: "", rollup_levels: %{})
+    socket = assign(socket, tezos_level: "", rollup_address: "", rollup_levels: %{}, outbox: [])
     if connected?(socket) do
       Process.send_after(self(), :constants, 100)
       Process.send_after(self(), :rollup_address, 100)
@@ -33,11 +33,19 @@ defmodule OutboxerWeb.PageLive do
   def handle_info(:rollup_level, socket) do
     block_time = socket.assigns.constants.block_time_ms
     levels = rollup_levels()
-    socket = assign(socket, rollup_levels: levels)
+    outbox = outbox_at(levels.finalised)
+
+    socket = assign(socket, rollup_levels: levels,
+                            outbox: new_outbox(socket.assigns.outbox, levels.finalised, outbox))
+
     Process.send_after(self(), :rollup_level, block_time)
 
     {:noreply, socket}
   end
+
+  def new_outbox([], level, messages), do: [{level, messages}]
+  def new_outbox([{prev, nil} | rest], level, messages), do: [{level, messages} | rest]
+  def new_outbox(outbox, level, messages), do: [{level, messages} | outbox]
 
   def handle_info(:rollup_address, socket) do
     address = rollup_address()
@@ -65,6 +73,12 @@ defmodule OutboxerWeb.PageLive do
     %HTTPoison.Response{body: body} = HTTPoison.get! "http://localhost:20010/global/block/cemented/state_current_level"
     cemented = Poison.decode! body
     %{finalised: finalised, cemented: cemented}
+  end
+
+  def outbox_at(level) do
+    %HTTPoison.Response{body: body} = HTTPoison.get! "http://localhost:20010/global/block/finalized/outbox/#{level}/messages"
+    messages = Poison.decode! body
+    if (not Enum.empty? messages), do: Poison.encode! messages
   end
 
   def rollup_address() do
