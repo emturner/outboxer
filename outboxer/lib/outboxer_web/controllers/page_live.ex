48e9df04ec969b0d870d163369c3c8556797404a
Emma Turner <mail@emturner.co.uk>
2023-10-19 14:06:55 +0100

    display rollup address

--- outboxer/lib/outboxer_web/controllers/page_live.ex
+++ outboxer/lib/outboxer_web/controllers/page_live.ex
@@ -2,8 +2,11 @@ defmodule OutboxerWeb.PageLive do
   use OutboxerWeb, :live_view
 
   def mount(_params, _conn, socket) do
-    socket = assign(socket, tezos_level: "")
-    if connected?(socket), do: Process.send_after(self(), :constants, 100)
+    socket = assign(socket, tezos_level: "", rollup_address: "")
+    if connected?(socket) do
+      Process.send_after(self(), :constants, 100)
+      Process.send_after(self(), :rollup_address, 100)
+    end
 
     {:ok, socket}
   end
@@ -11,7 +14,9 @@ defmodule OutboxerWeb.PageLive do
   def handle_info(:constants, socket) do
     proto_constants = proto_constants()
     socket = assign(socket, constants: proto_constants)
+
     Process.send_after(self(), :tezos_level, 100)
+
     {:noreply, socket}
   end
 
@@ -20,6 +25,13 @@ defmodule OutboxerWeb.PageLive do
     level = tezos_level()
     socket = assign(socket, tezos_level: level)
     Process.send_after(self(), :tezos_level, block_time)
+
+    {:noreply, socket}
+  end
+
+  def handle_info(:rollup_address, socket) do
+    address = rollup_address()
+    socket = assign(socket, rollup_address: address)
 
     {:noreply, socket}
   end
@@ -35,5 +47,10 @@ defmodule OutboxerWeb.PageLive do
     %HTTPoison.Response{body: body} = HTTPoison.get! "http://localhost:20000/chains/main/blocks/head-1/header/shell"
     %{"level" => level} = Poison.decode! body
     level
+  end
+
+  def rollup_address() do
+    %HTTPoison.Response{body: address} = HTTPoison.get! "http://localhost:20010/global/smart_rollup_address"
+    Poison.decode! address
   end
 end
